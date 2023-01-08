eceb995fc658592ef5353692c62d9efcde27239d
selcuklu <yilmax4@gmail.com>
2023-01-08 22:20:52 +0300

    lesson 10 dahil ok

--- SYBLOG/app.js
+++ SYBLOG/app.js
@@ -1,20 +1,29 @@
 const path = require("path");
 const express = require("express");
+const exphbs =require("express-handlebars")
 const app = express();
 const port = 3000;
 const hostname = "127.0.0.1";
 
 app.use(express.static('public'))
-
-app.engine("handlebars", engine());
+// engine() yerine exphbs() kullandı
+app.engine("handlebars", exphbs.engine());
 app.set("view engine", "handlebars");
-app.set("views", "./views");
+// app.set("views", "./views");
 
 app.get("/",(req,res)=>{
   // res.sendFile(path.resolve(__dirname,"site/index.html"))
   res.render("site/index");
 })
 
+app.get("/about", (req, res) => {
+  // res.sendFile(path.resolve(__dirname,"site/about.html"))
+  res.render("site/about");
+});
+app.get("/blog", (req, res) => {
+  // res.sendFile(path.resolve(__dirname,"site/about.html"))
+  res.render("site/blog");
+});
 
 
 app.listen(port, hostname, () => {
