f1b163d2aa86c1ca11d79ce6f8b6c1859e3a1260
selcuklu <yilmax4@gmail.com>
2023-01-09 20:12:17 +0300

    changed style css

--- SYBLOG/app.js
+++ SYBLOG/app.js
@@ -6,7 +6,7 @@ const port = 3000;
 const hostname = "127.0.0.1";
 
 app.use(express.static('public'))
-// engine() yerine exphbs() kullandı
+
 app.engine("handlebars", exphbs.engine());
 app.set("view engine", "handlebars");
 // app.set("views", "./views");
