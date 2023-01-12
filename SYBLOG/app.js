481e011827c96a65954ad65202ccc3579e02da6a
selcuklu <yilmax4@gmail.com>
2023-01-12 22:15:03 +0300

    was added routes

--- SYBLOG/app.js
+++ SYBLOG/app.js
@@ -1,4 +1,4 @@
-const path = require("path");
+// const path = require("path");  //!buna gerek kalmadı.
 const express = require("express");
 const exphbs =require("express-handlebars")
 const app = express();
@@ -16,34 +16,40 @@ app.engine("handlebars", exphbs.engine());
 app.set("view engine", "handlebars");
 // app.set("views", "./views");
 
-app.get("/",(req,res)=>{
-  // res.sendFile(path.resolve(__dirname,"site/index.html"))
-  res.render("site/index");
-})
-
-app.get("/about", (req, res) => {
-  // res.sendFile(path.resolve(__dirname,"site/about.html"))
-  res.render("site/about");
-});
-app.get("/blog", (req, res) => {
-  // res.sendFile(path.resolve(__dirname,"site/about.html"))
-  res.render("site/blog");
-});
-app.get("/contact", (req, res) => {
-  // res.sendFile(path.resolve(__dirname,"site/about.html"))
-  res.render("site/contact");
-});
-app.get("/login", (req, res) => {
-  // res.sendFile(path.resolve(__dirname,"site/about.html"))
-  res.render("site/login");
-});
-app.get("/register", (req, res) => {
-  // res.sendFile(path.resolve(__dirname,"site/about.html"))
-  res.render("site/register");
-});
-
-
 app.listen(port, hostname, () => {
   console.log(`Server calisti mi, http://${hostname}:${port}/`);
 });
+
+const main = require('./routes/main')
+app.use('/',main)
+
+//!bu bölümü routes a taşıdık below yerine 23. ve 24. satıra yazılana bak
+// app.get("/",(req,res)=>{
+//   // res.sendFile(path.resolve(__dirname,"site/index.html"))
+//   res.render("site/index");
+// })
+
+// app.get("/about", (req, res) => {
+//   // res.sendFile(path.resolve(__dirname,"site/about.html"))
+//   res.render("site/about");
+// });
+// app.get("/blog", (req, res) => {
+//   // res.sendFile(path.resolve(__dirname,"site/about.html"))
+//   res.render("site/blog");
+// });
+// app.get("/contact", (req, res) => {
+//   // res.sendFile(path.resolve(__dirname,"site/about.html"))
+//   res.render("site/contact");
+// });
+// app.get("/login", (req, res) => {
+//   // res.sendFile(path.resolve(__dirname,"site/about.html"))
+//   res.render("site/login");
+// });
+// app.get("/register", (req, res) => {
+//   // res.sendFile(path.resolve(__dirname,"site/about.html"))
+//   res.render("site/register");
+// });
+
+
+
 
---
+++ SYBLOG/routes/main.js
@@ -0,0 +1,34 @@
+const express = require("express");
+
+const router=express.Router()
+
+//! app yazan yerleri router ile değiştirdik.
+router.get("/", (req, res) => {
+  // res.sendFile(path.resolve(__dirname,"site/index.html"))
+  res.render("site/index");
+});
+
+router.get("/about", (req, res) => {
+  // res.sendFile(path.resolve(__dirname,"site/about.html"))
+  res.render("site/about");
+});
+router.get("/blog", (req, res) => {
+  // res.sendFile(path.resolve(__dirname,"site/about.html"))
+  res.render("site/blog");
+});
+router.get("/contact", (req, res) => {
+  // res.sendFile(path.resolve(__dirname,"site/about.html"))
+  res.render("site/contact");
+});
+router.get("/login", (req, res) => {
+  // res.sendFile(path.resolve(__dirname,"site/about.html"))
+  res.render("site/login");
+});
+router.get("/register", (req, res) => {
+  // res.sendFile(path.resolve(__dirname,"site/about.html"))
+  res.render("site/register");
+});
+
+
+
+module.exports=router
