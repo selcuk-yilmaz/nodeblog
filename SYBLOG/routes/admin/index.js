8a677bfce2d5066332116c2f154ffe38a63476e1
selcuklu <yilmax4@gmail.com>
2023-01-27 21:26:18 +0300

    admin page carry on

--- SYBLOG/routes/admin/index.js
+++ SYBLOG/routes/admin/index.js
@@ -1,34 +1,45 @@
 const express = require("express");
 const router = express.Router();
-const Category = require("../../models/Category")
+const Category = require("../../models/Category");
+const Post = require("../../models/Post")
 
 router.get("/", (req, res) => {
   res.render("site-admin/index");
 });
 
 router.get("/categories", (req, res) => {
-  Category.find({}).sort({$natural:-1}).lean().then(categories =>{
-    res.render("site-admin/categories",{categories:categories});
-  })
- 
+  Category.find({})
+    .sort({ $natural: -1 })
+    .lean()
+    .then((categories) => {
+      res.render("site-admin/categories", { categories: categories });
+    });
 });
 
 router.post("/categories", (req, res) => {
-  Category.create(req.body,(error,category)=>{
-    if(!error){
-     res.redirect("categories"); 
+  Category.create(req.body, (error, category) => {
+    if (!error) {
+      res.redirect("categories");
     }
-    
-  })
+  });
 });
 
 router.delete("/categories/:id", (req, res) => {
-Category.remove({_id : req.params.id}).then(()=>{
-   res.redirect("/admin/categories"); 
-})
+  Category.remove({ _id: req.params.id }).then(() => {
+    res.redirect("/admin/categories");
+  });
 });
 
-
-
+router.get("/posts", (req, res) => {
+    Post.find({})
+    .populate({ path: "category", model: Category })
+    .sort({ $natural: -1 })
+    .lean()
+    .then((posts) => {
+     
+          res.render("site-admin/posts", {posts: posts});
+        });
+  
+});
 
 module.exports = router;
